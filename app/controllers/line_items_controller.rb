8ccab56929043ec589c9327394b72bde718b100f
kosjan199 <akishe14@gmail.com>
2020-08-19 15:51:59 +0400

    Add AJAX vision to cart and new style to block cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -30,8 +30,11 @@ class LineItemsController < ApplicationController
     @line_item = @cart.add_product(product.id)
 
     respond_to do |format|
-      if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: 'Товар добавлен в корзину' }
+      if @line_item.save  
+             
+        format.html { redirect_to store_url, notice: 'Товар добавлен в корзину'  }
+        format.js { @current_item = @line_item }
+
        else
         format.html { render :new }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
